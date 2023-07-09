90ad4c7e71678541b783e74b6d3d3e99312db7b8
Siwalik Mukherjee <20061595+siwalikm@users.noreply.github.com>
2023-07-09 21:16:16 +0200

    update example

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface User {
+  id: number;
+  name: string;
+  avatar: string;
+  hobbies: Array<string>;
+  theme: string;
+}
+
+const getData = async () => {
+  const response = await fetch(
+    "https://my-json-server.typicode.com/siwalikm/demo-proxyman-app/users/"
+  );
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [usersList, setUsersList] = useState<User[]>([]);
+
+  useEffect(() => {
+    getData().then((data) => setUsersList(data));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid-container">
+        {usersList.map((user) => (
+          <div
+            className="grid-item"
+            key={user.id}
+            style={{ background: user.theme }}
+          >
+            <img src={user.avatar} alt={user.name} />
+            <h2>{user.name}</h2>
+            {user.hobbies.length > 0 && (
+              <div className="hobby">
+                <ul>
+                  {user?.hobbies.map((hobby) => (
+                    <li key={hobby}>{hobby}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
